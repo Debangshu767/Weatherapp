28eb6c318b35eb6846f6e131b991577e7f0a868c
Debangshu <debangshu182002@gmail.com>
2021-09-02 21:50:43 +0530

    first commit

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -3,7 +3,7 @@ const key = 'tM5eS4UGGQ8kZkaSdOdzw3qixvTCMz3g';
 //weather info
 
 const getWeather = async (id) =>{
-    const base =  'http://dataservice.accuweather.com/currentconditions/v1/'
+    const base =  'https://dataservice.accuweather.com/currentconditions/v1/'
     const query = `${id}?apikey=${key}`;
 
     const response = await fetch(base+query);
@@ -16,7 +16,7 @@ const getWeather = async (id) =>{
 
 //city info
 const getCity = async (city) => {
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const base = 'https://dataservice.accuweather.com/locations/v1/cities/search';
     const query = `?apikey=${key}&q=${city}`;
 
     const response = await fetch(base + query);
